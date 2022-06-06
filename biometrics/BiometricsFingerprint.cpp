7339b0a5a863c04d51321a086d8081ee1afffc5f
arturo51236 <rtsanonynousperson@gmail.com>
2022-06-06 18:14:39 +0200

    Merge apollon into sm8250-common
    * From https://github.com/LineageOS/android_device_xiaomi_apollon/tree/48f108d2267be5299546eb4f12461c65ed04c4aa
    
    - Drop FOD (apollo have an IPS display)
    - Drop camera motor (lmi specific)
    - Drop A/B stuff (apollo variants are non-VAB)
    - Moved prop files to props folder
    
    Signed-off-by: Arturo <rtsanonynousperson@gmail.com>

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -14,15 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.xiaomi_kona"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.xiaomi_kona"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.apollon"
+#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.apollon"
 
 #include <hardware/hw_auth_token.h>
 
 #include "xiaomi_fingerprint.h"
 #include "BiometricsFingerprint.h"
 
-#include <cutils/properties.h>
 #include <inttypes.h>
 #include <unistd.h>
 
@@ -39,10 +38,7 @@ static const uint16_t kVersion = HARDWARE_MODULE_API_VERSION(2, 1);
 // List of fingerprint HALs
 static const char *kHALClasses[] = {
     "fpc",
-    "fpc_fod",
     "goodix",
-    "goodix_fod",
-    "goodix_fod6",
 };
 
 using RequestStatus =
@@ -58,7 +54,6 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
             ALOGE("Can't open HAL module, class %s", class_name);
         } else {
             ALOGI("Opened fingerprint HAL, class %s", class_name);
-            property_set("persist.vendor.sys.fp.vendor", class_name); // fix AliPay TouchID
             break;
         }
     }
